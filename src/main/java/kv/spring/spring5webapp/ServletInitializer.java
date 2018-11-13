5a256a1ccdf965455b211d6faf5a3b2ddabdd377
vi <dev.facereal@gmail.com>
2018-11-13 13:25:05 +0700

    011 Open Project in IntelliJ.mp4 | 05m22s

---
+++ src/main/java/kv/spring/spring5webapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package kv.spring.spring5webapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Spring5webappApplication.class);
+    }
+
+}
