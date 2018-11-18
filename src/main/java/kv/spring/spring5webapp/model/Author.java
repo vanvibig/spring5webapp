334140bd7ea51876d230c5c0cf87cb2169fca830
vi <dev.facereal@gmail.com>
2018-11-18 09:26:43 +0700

    013 Equality in Hibernate.mp4

--- src/main/java/kv/spring/spring5webapp/model/Author.java
+++ src/main/java/kv/spring/spring5webapp/model/Author.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Data
@@ -29,5 +30,18 @@ public class Author {
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return id == author.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
